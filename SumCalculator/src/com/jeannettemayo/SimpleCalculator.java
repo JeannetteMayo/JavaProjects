9b5e5162378bcf55daec70beb95e32ca21908743
Jeannette Mayo <65695953+Jgallegos0@users.noreply.github.com>
2021-05-13 23:48:26 -0700

    Add files via upload
    Simple Calculator (Addition, Subtraction, Division, Multiplication) using Object Oriented Programing.

---
+++ SumCalculator/src/com/jeannettemayo/SimpleCalculator.java
@@ -0,0 +1,42 @@
+package com.jeannettemayo;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        //
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        //
+        return this.secondNumber;
+    }
+    public void setFirstNumber( double f_Number) {
+        this.firstNumber = f_Number;
+    }
+    public void setSecondNumber(double s_Number) {
+        this.secondNumber = s_Number;
+    }
+    public double getAdditionResult() {
+        double sum = firstNumber + secondNumber;
+        return sum;
+    }
+    public double getSubtractionResult() {
+        double sub = firstNumber - secondNumber;
+        return sub;
+    }
+    public double getMultiplicationResult() {
+        double mult = firstNumber * secondNumber;
+        return mult;
+    }
+
+    public double getDivisionResult() {
+        double div = firstNumber / secondNumber;
+        if(secondNumber == 0) {
+            return 0;
+        }
+        return div;
+    }
+}
