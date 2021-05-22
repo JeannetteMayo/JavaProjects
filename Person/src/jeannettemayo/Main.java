e4e6131fdf33330153e086f1b6e5e83133669d29
Jeannette Mayo <65695953+JeannetteMayo@users.noreply.github.com>
2021-05-21 22:55:26 -0700

    Update Main.java

--- Person/src/jeannettemayo/Main.java
+++ Person/src/jeannettemayo/Main.java
@@ -2,10 +2,13 @@ package jeannettemayo;
 
 public class Main {
     public static void main(String[] args) {
-        // Only one object needs to be created in this case to use the class
+        // Only one object needs to be created to use the Person() class.
         Person name = new Person();
+        /* Call setFirstName() and setLastName() methods from the 'name' object
+        that was created */
         name.setFirstName("Your");
         name.setLastName("Name");
+        // Print full name
         System.out.println(name.getFullName());
 
     }
